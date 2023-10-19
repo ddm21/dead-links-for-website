2167e16f9643dd214a975ffb33e4d9445c8a4edf
ddm21 <98445934+ddm21@users.noreply.github.com>
2023-10-19 10:22:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import requests
 import openpyxl
 from openpyxl.styles import Font
 from bs4 import BeautifulSoup
-from tqdm import tqdm  # Import tqdm for the progress bar
+from tqdm import tqdm
 
 # Function to check if a link is alive
 def is_link_alive(url):
@@ -34,9 +34,10 @@ workbook = openpyxl.Workbook()
 worksheet = workbook.active
 
 # Set the headers for the Excel file
-worksheet['A1'] = 'Element'
-worksheet['B1'] = 'Link'
-worksheet['C1'] = 'Status'
+worksheet['A1'] = 'Page URL'
+worksheet['B1'] = 'Element'
+worksheet['C1'] = 'Link'
+worksheet['D1'] = 'Status'
 
 # Style the header row
 font = Font(bold=True)
@@ -47,20 +48,25 @@ for cell in worksheet['1:1']:
 row = 2
 
 # Check links on the website
-links_to_check = get_links_from_page(website_url)
+pages_to_check = [website_url]
 
 # Use tqdm to create a progress bar
-with tqdm(total=len(links_to_check), unit='link') as pbar:
-    for link in links_to_check:
-        if is_link_alive(link):
-            status = '✔️'
-        else:
-            status = '❌'
-        worksheet.cell(row=row, column=1, value='Link')
-        worksheet.cell(row=row, column=2, value=link)
-        worksheet.cell(row=row, column=3, value=status)
-        row += 1
-        pbar.update(1)  # Update the progress bar
+with tqdm(total=len(pages_to_check), unit='page') as page_pbar:
+    for page_url in pages_to_check:
+        links_to_check = get_links_from_page(page_url)
+        with tqdm(total=len(links_to_check), unit='link', leave=False) as link_pbar:
+            for link in links_to_check:
+                if is_link_alive(link):
+                    status = '✔️'
+                else:
+                    status = '❌'
+                worksheet.cell(row=row, column=1, value=page_url)
+                worksheet.cell(row=row, column=2, value='Link')
+                worksheet.cell(row=row, column=3, value=link)
+                worksheet.cell(row=row, column=4, value=status)
+                row += 1
+                link_pbar.update(1)
+        page_pbar.update(1)
 
 # Save the Excel file
 workbook.save('dead_links_report.xlsx')
